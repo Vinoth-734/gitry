c0d4f8f523a5d4c2e682b9d85756e93f3d931a30
name <venovenoth2004@gmail.com>
2025-05-27 12:36:30 +0530

    Revert "[Two]1cmt"
    This reverts commit e445316d59a0f2e054bd4ab599b707ec096933c9.

--- gitry/Program.cs
+++ gitry/Program.cs
@@ -11,9 +11,7 @@ namespace gitry
             //one more from master
             //from branch 1
             //from sub branch of one
-            
 
-            //This is for checkng revert in two 
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
